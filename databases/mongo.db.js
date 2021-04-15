d9b386bb415f0055630d0088e9f7877ca774338f
ankitOneNineNineNine <pradhanankit12@gmail.com>
2021-04-15 20:42:55 +0545

    changes

--- databases/mongo.db.js
+++ databases/mongo.db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // const mongoURI = 'mongodb://localhost:27017/WINEAM'
 
-const mongoURI = 'mongodb+srv://ankitProj:<password>@cluster0.v1cqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://ankitProj:BARCA@nkit$980@cluster0.v1cqa.mongodb.net/WINEAM?retryWrites=true&w=majority'
 
 mongoose.connect(`${mongoURI}`,{ useNewUrlParser: true,   useUnifiedTopology: true  }, function(err,connect){
     if(err){
