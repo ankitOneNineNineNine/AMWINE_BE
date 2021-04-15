87085e30153d7584675ff47be1d6939b686ca04f
ankitOneNineNineNine <pradhanankit12@gmail.com>
2021-04-15 21:18:26 +0545

    changes

--- databases/mongo.db.js
+++ databases/mongo.db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 // const mongoURI = 'mongodb://localhost:27017/WINEAM'
 
-const mongoURI = 'mongodb+srv://ankitProj:BARCA@nkit$980@cluster0.v1cqa.mongodb.net/WINEAM?retryWrites=true&w=majority'
+const mongoURI = ''
 
-mongoose.connect(`${mongoURI}`,{ useNewUrlParser: true,   useUnifiedTopology: true  }, function(err,connect){
+mongoose.connect(process.env.mongoURI || `${mongoURI}`,{ useNewUrlParser: true,   useUnifiedTopology: true  }, function(err,connect){
     if(err){
         console.log(err)
     }
--- databases/redis.db.js
+++ databases/redis.db.js
@@ -1,8 +1,8 @@
 const redis = require("redis");
+var url = require('url');
+var redisURL = url.parse(process.env.redisURI);
+var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 
-const redisClient = redis.createClient(
-  process.env.REDISCLOUD_URL, {no_ready_check: true}
-)
 redisClient.on("connect", function(){
     console.log("Connected to Redis Client")
 })
