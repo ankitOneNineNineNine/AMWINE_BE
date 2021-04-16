7992dc362d2fedf9d44954131a49a6a5bcf9b166
ankitOneNineNineNine <pradhanankit12@gmail.com>
2021-04-16 07:55:32 +0545

    changes

--- databases/redis.db.js
+++ databases/redis.db.js
@@ -1,11 +1,15 @@
 const redis = require("redis");
 
+var redisClient = redis.createClient({
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    no_ready_check: true ,
+    password: process.env.REDIS_PASS
+}
+);
 
-
-var redisClient = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST, {no_ready_check: true}, {auth_pass: process.env.REDIS_PASS});
-
-redisClient.on("connect", function(){
-    console.log("Connected to Redis Client")
-})
+redisClient.on("connect", function () {
+  console.log("Connected to Redis Client");
+});
 
 module.exports = redisClient;
